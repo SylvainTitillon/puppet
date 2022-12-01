810baf15d8bf0887afdf7f0e4411abb492fbca0d
SylvainTitillon <62641980+SylvainTitillon@users.noreply.github.com>
2022-12-01 15:43:52 +0100

    Delete puppet directory

--- puppet/manifests/premier_manisfest.pp
+++
@@ -1,3 +0,0 @@
-file {'/tmp/formation':
- content => 'Hello world',
-}
--- puppet/user.pp
+++
@@ -1,6 +0,0 @@
-# Définition basique d'un utilisateur
-user { 'sylvain':
-  ensure   => 'present',
-  comment  => 'Créé via puppet',
-  shell    => '/bin/zsh',
-}
